b8a147a776cb332a8f11aaee9218e972df943c88
MechaEdgar <ee1206@hotmail.com>
2016-10-26 16:13:19 -0500

    Add Intents and OnCLick Listener.
    Added onclick listener on main method.

---
+++ QuizzApp2/app/src/main/java/com/example/android/quizzapp2/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.android.quizzapp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import static android.os.Build.VERSION_CODES.M;
+
+public class MainActivity extends AppCompatActivity {
+
+    int totalScore = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void scoreView(View view) {
+        //Check if the first answer is correct
+        RadioButton firstAnswer = (RadioButton) findViewById(R.id.correctAnswerOne);
+        boolean firstCorrect = firstAnswer.isChecked();
+
+        //Check if the Second first checkbox answer is correct
+        CheckBox secondAnswer = (CheckBox) findViewById(R.id.correctAnswerTwo);
+        boolean secondCorrect = secondAnswer.isChecked();
+
+        //Check if the Second checkbox answer is correct
+        CheckBox secondCorrectAnswer = (CheckBox) findViewById(R.id.secondCorrectAnswer);
+        boolean secondCorrectAns = secondCorrectAnswer.isChecked();
+
+        //Check if the third answer is correct
+        RadioButton thirdAnswer = (RadioButton) findViewById(R.id.correctAnswerThree);
+        boolean thirdCorrect = thirdAnswer.isChecked();
+
+        //Check if the fourth answer is correct
+        EditText fourthAnswer = (EditText) findViewById(R.id.correctAnswerFour);
+        String fourthCorrect = fourthAnswer.getText().toString();
+        //Check if the fifth answer is correct
+        RadioButton fifthAnswer = (RadioButton) findViewById(R.id.correctAnswerFive);
+        boolean fifthCorrect = fifthAnswer.isChecked();
+
+        checkAnswer1(firstCorrect);
+        checkAnswer2(secondCorrect, secondCorrectAns);
+        checkAnswer3(thirdCorrect);
+        checkAnswer4(fourthCorrect);
+        checkAnswer5(fifthCorrect);
+        Toast.makeText(this, "Your score is: " + totalScore, Toast.LENGTH_SHORT).show();
+        totalScore = 0;
+
+    }
+
+    public int checkAnswer1(boolean firstCorrect) {
+        if (firstCorrect) {
+            totalScore += 1;
+
+        } else {
+            return totalScore;
+        }
+
+        return totalScore;
+    }
+
+    public int checkAnswer2(boolean secondCorrect, boolean secondCorrectAns) {
+        if (secondCorrect && secondCorrectAns) {
+            totalScore += 1;
+
+        } else {
+            return totalScore;
+        }
+
+        return totalScore;
+    }
+
+    public int checkAnswer3(boolean thirdCorrect) {
+        if (thirdCorrect) {
+            totalScore += 1;
+
+        } else {
+            return totalScore;
+        }
+
+        return totalScore;
+    }
+
+    public int checkAnswer4(String fourthCorrect) {
+        String relative = "RELATIVE";
+        if (fourthCorrect.equals(relative)) {
+            totalScore += 1;
+
+        } else {
+            return totalScore;
+        }
+
+        return totalScore;
+    }
+
+    public int checkAnswer5(boolean fifthCorrect) {
+        if (fifthCorrect) {
+            totalScore += 1;
+
+        } else {
+            return totalScore;
+        }
+
+        return totalScore;
+    }
+
+
+}
--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,8 +15,12 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +30,99 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        //Find the View that shows the numbers category
+        TextView numbers= (TextView) findViewById(R.id.numbers);
+
+        //Set a clickListener on that View.
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Make a Toast that shows what view are you opening
+                Toast.makeText(v.getContext(),"Open the list of numbers", Toast.LENGTH_SHORT).show();
+                //Create a new intent to open {@link NumbersActivity}
+                Intent numbersIntent = new Intent(MainActivity.this,
+                        NumbersActivity.class);
+
+                //Start the new activity
+                startActivity(numbersIntent);
+            }
+        });
+
+        //Find the View that shows the colors category
+        TextView colors = (TextView) findViewById(R.id.colors);
+
+        //Set a clickListener on colors
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Make a Toast that shows what view are you opening
+                Toast.makeText(v.getContext(),"Open the list of colors", Toast.LENGTH_SHORT).show();
+                //Create a new intent to open {@link NumbersActivity}
+                Intent colorsIntent = new Intent(MainActivity.this,
+                        ColorsActivity.class);
+
+                //Start the new activity
+                startActivity(colorsIntent);
+            }
+        });
+
+        // Find the view that shows the family category
+        TextView family = (TextView) findViewById(R.id.family);
+
+        //Set a clickListener on colors
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Make a Toast that shows what view are you opening
+                Toast.makeText(v.getContext(),"Open the family list", Toast.LENGTH_SHORT).show();
+                //Create a new intent to open {@link FamilyActivity}
+                Intent familyIntent = new Intent(MainActivity.this,
+                        FamilyActivity.class);
+                //Start family activity
+                startActivity(familyIntent);
+            }
+        });
+
+        // Find the view that shows the phrases category
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+        // Set a clickListener to phrases
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Make a Toast that shows what view are you opening
+                Toast.makeText(v.getContext(),"Open Phrases list", Toast.LENGTH_SHORT).show();
+
+                //Create a new intent to open {@link PhrasesActivity}
+                Intent phrasesIntent = new Intent(MainActivity.this,
+                        PhrasesActivity.class);
+                //Start Phrases activity
+                startActivity(phrasesIntent);
+            }
+        });
     }
+
+ /*   public void openNumbersList(View view) {
+        // Intent to launch NumbersActivity
+        Intent i = new Intent(this, NumbersActivity.class);
+        startActivity(i);
+    }
+
+    public void  openFamilyMembersList(View view) {
+        // Intent to launch FamilyActivity
+        Intent i =  new Intent(this, FamilyActivity.class);
+        startActivity(i);
+    }
+
+    public void openColorsList(View view){
+        //Intent to launch ColorsActivity
+        Intent i = new Intent(this, ColorsActivity.class);
+        startActivity(i);
+    }
+
+    public void openPhrasesList(View view){
+        //Intent to launch PhrasesActivity
+        Intent i = new Intent(this,PhrasesActivity.class);
+        startActivity(i);
+    } */
 }
